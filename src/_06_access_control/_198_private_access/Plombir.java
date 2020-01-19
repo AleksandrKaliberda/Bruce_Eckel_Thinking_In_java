b1663e6c8a0b32f414e0374827d66481c8886d78
AleksandrKaliberda <alexandr.kaliberda@gmail.com>
2020-01-19 16:42:08 +0200

    '_198_private_access'

---
+++ src/_06_access_control/_198_private_access/Plombir.java
@@ -0,0 +1,24 @@
+package _06_access_control._198_private_access;
+
+class Plombir {
+
+    //Plombir object creation is blocked.
+    private Plombir(){};
+
+    //Plombir object creation by method
+    static Plombir makePlombir(){
+        return new Plombir();
+    }
+
+}
+
+class IceCream{
+    public static void main(String[] args) {
+
+        //Plombir p = new Plombir(); //Cannot create and object Plombir.
+
+        Plombir p1 = Plombir.makePlombir();
+        System.out.println("p1: "+ p1);
+
+    }
+}
