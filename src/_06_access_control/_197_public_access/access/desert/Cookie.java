66dcbe657782da6df201f348aa3c63a69fdb6499
AleksandrKaliberda <alexandr.kaliberda@gmail.com>
2020-01-19 15:59:21 +0200

    'public_access_and_package_access_sample'

---
+++ src/_06_access_control/_197_public_access/access/desert/Cookie.java
@@ -0,0 +1,10 @@
+package _06_access_control._197_public_access.access.desert;
+
+//: access/dessert/Cookie.java
+// Creates a library.
+public class Cookie {
+    public Cookie() {
+        System.out.println("Cookie constructor");
+    }
+    void bite() { System.out.println("bite"); }
+} ///:~
